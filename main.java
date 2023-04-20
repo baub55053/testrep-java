e99b82a8f7dd61f3983ff2dd25e0bc8d62c4199b
baub55053 <131408126+baub55053@users.noreply.github.com>
2023-04-21 00:28:42 +0300

    Create main.java
    2 ways of sorting

---
+++ main.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class MatrixSort {
+
+    public static void main(String[] args) {
+        int[][] matrix = {{4, 2, 9}, {8, 1, 5}, {3, 7, 6}};
+
+        // Sort in ascending row order
+        long startTime = System.nanoTime();
+        Arrays.sort(matrix, Comparator.comparingInt(a -> a[0]));
+        long endTime = System.nanoTime();
+        System.out.println("Sorted in ascending row order:");
+        printMatrix(matrix);
+        System.out.println("Time taken: " + (endTime - startTime) + " nanoseconds");
+
+        // Sort in descending row order
+        startTime = System.nanoTime();
+        Arrays.sort(matrix, Comparator.comparingInt((int[] a) -> a[0]).reversed());
+        endTime = System.nanoTime();
+        System.out.println("Sorted in descending row order:");
+        printMatrix(matrix);
+        System.out.println("Time taken: " + (endTime - startTime) + " nanoseconds");
+      
+    }
+
+    public static void printMatrix(int[][] matrix) {
+        for (int[] row : matrix) {
+            System.out.println(Arrays.toString(row));
+        }
+    }
+}
